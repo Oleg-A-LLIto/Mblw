92dbda37e8a4e78efe482c703c90b4100f524372
Oleg-A-LLIto <43452849+Oleg-A-LLIto@users.noreply.github.com>
2021-01-12 02:02:47 +0300

    added js

---
+++ random_list.js
@@ -0,0 +1,31 @@
+function prices() {
+	var prices = document.getElementsByTagName("pricetag");
+	for(var i=0; i<prices.length; i++){
+		prices.item(i).innerHTML = Math.round((Math.random()*5+5)*100)/100 + "$";
+	}
+}
+		
+function names() {
+	var first_names = ['Funny', 'Stubborn', 'Clumsy', 'Cute', 'Curious', 'Adorable', 'Happy', 'Weird', 'Small', 'Friendly', 'Fluffy', 'Little'];
+	var last_names = ['boi', 'sir', 'ball', 'lady', 'bun'];
+	var names = document.getElementsByTagName("nametag");	
+	for(var i = 0; i<names.length; i++){
+		names.item(i).innerHTML = first_names[Math.floor(Math.random() * first_names.length)] +" " + last_names[Math.floor(Math.random() * last_names.length)];
+		
+	}
+}
+
+function pictures() {
+	var picnumber = 19;
+	var pics = document.getElementsByClassName("card_img");
+	for(var i=0; i<pics.length; i++){
+		pics.item(i).src = "images/toy/base/ ("+Math.ceil(Math.random()*picnumber)+").jpg";
+	}
+}
+		
+window.onload = function(){
+	alert("mblw");
+	prices();
+	names();
+	pictures()
+}
